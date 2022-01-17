b8c4464945aa09c962fb160167493531a1681a47
KacperHolubowicz <kac.perhol2000@gmail.com>
2022-01-17 13:32:32 +0100

    Fixed password validation while editing

--- WebApplication/ViewModels/ServicePassword/PasswordEditVM.cs
+++ WebApplication/ViewModels/ServicePassword/PasswordEditVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.ViewModels.Attributes;
 
 namespace WebApplication.ViewModels.ServicePassword
 {
@@ -16,6 +17,7 @@ namespace WebApplication.ViewModels.ServicePassword
 
         [Required]
         [Display(Name = "New password for that service")]
+        [StrongPassword(8, 28)]
         public string Password { get; set; }
 
         [Required]

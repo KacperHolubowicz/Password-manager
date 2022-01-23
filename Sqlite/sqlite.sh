f2b2ce6291f37a43ddb1bd967c166b93bfbee1ff
KacperHolubowicz <kac.perhol2000@gmail.com>
2022-01-23 12:52:07 +0100

    Changed path for db

--- Sqlite/sqlite.sh
+++ Sqlite/sqlite.sh
@@ -1,2 +1,2 @@
 sudo apt -q install -y sqlite3 #na wypadek braku sqlite
-sqlite3 Sqlite/password.db ".read Sqlite/initdb.sql" #czytanie schemy bazy z pliku
+sqlite3 /tmp/password.db ".read Sqlite/initdb.sql" #czytanie schemy bazy z pliku
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
+# Skrypt pisany pod linux Mint, zatem powinien zadzialac rowniez na ubuntu
+# Skrypt uruchamiajacy baze sqlite w katalogu /tmp
+
 echo "Initializing sqlite3 database..."
 sh Sqlite/sqlite.sh
 echo "Finished initializing database!"
-echo "Initializing web app docker image..."
-sudo docker build 
+
+sudo apt install dotnet-sdk-5.0 # sdk frameworka .net 5.0, POWINIEN zawierac runtime do asp.net core
+dotnet run --project WebApp/WebApplication/WebApplication.csproj
